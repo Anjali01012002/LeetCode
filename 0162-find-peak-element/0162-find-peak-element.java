dabf8b1af0341d758444b142742e8386fcd60e6b
Anjali Kumari <137891338+Anjali01012002@users.noreply.github.com>
2024-04-04 18:34:45 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (28.47%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start < end){
+            
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > arr[mid+1]){
+                //you are in decreasing part of array
+                //his may be the answer, but look at left
+                //this is why end is not equal to mid-1
+                end = mid;
+            }else{
+                //you are in decreasing part of array
+                start = mid + 1; //because we know that mid+1 element> mid element
+            }
+        }
+        
+        //in the end, start==end and pointing to the largest number because of 2 checks above
+        return start;  //return end as both are equal
+    }
+}
