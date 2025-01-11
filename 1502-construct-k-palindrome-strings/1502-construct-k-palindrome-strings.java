fac0605b74e7be4c974fc867257acf3add784c15
Anjali Kumari <137891338+Anjali01012002@users.noreply.github.com>
2025-01-11 09:08:20 +0530

    Time: 3 ms (100%), Space: 45.4 MB (61.15%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(k>s.length()){
+            return false;
+        }
+
+        int[] charCount=new int[26];
+        for(char c:s.toCharArray()){
+            charCount[c-'a']++;
+        }
+
+        int oddCount=0;
+        for(int count:charCount){
+            if(count%2==1){
+                oddCount++;
+            }
+        }
+
+        return k>=oddCount;
+    }
+}
